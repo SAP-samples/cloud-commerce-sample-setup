574d6e7bd7f164383a7317f78c7252da7bd7f10c
Tyler MacWilliam <tyler.macwilliam@sap.com>
2020-05-01 11:03:12 -0700

    update nguniversal/express-engine

--- js-storefront/spartacusstore/src/app/app.server.module.ts
+++ js-storefront/spartacusstore/src/app/app.server.module.ts
@@ -3,7 +3,6 @@ import { ServerModule, ServerTransferStateModule } from '@angular/platform-serve
 
 import { AppModule } from './app.module';
 import { AppComponent } from './app.component';
-import { ModuleMapLoaderModule } from '@nguniversal/module-map-ngfactory-loader';
 
 @NgModule({
   imports: [
