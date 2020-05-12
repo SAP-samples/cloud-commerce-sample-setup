3ede0cfeac76de44751e24e18fefbdd334090f6f
Tyler MacWilliam <tyler.macwilliam@sap.com>
2020-05-12 15:51:55 -0700

    schematics changes

--- js-storefront/spartacusstore/server.ts
+++ js-storefront/spartacusstore/server.ts
@@ -1,3 +1,7 @@
+/***************************************************************************************************
+ * Load `$localize` onto the global scope - used if i18n tags appear in Angular templates.
+ */
+import '@angular/localize/init';
 import 'zone.js/dist/zone-node';
 
 import { ngExpressEngine } from '@nguniversal/express-engine';
