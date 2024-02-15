0beb8a11bd695c54674464cac6175dd2ce9377b9
test <test>
2024-02-15 15:29:31 -0500

    init

--- js-storefront/b2bspastore/src/app/app.component.spec.ts
+++ js-storefront/b2bspastore/src/app/app.component.spec.ts
@@ -26,6 +26,6 @@ describe('AppComponent', () => {
     const fixture = TestBed.createComponent(AppComponent);
     fixture.detectChanges();
     const compiled = fixture.nativeElement as HTMLElement;
-    expect(compiled.querySelector('.content span')?.textContent).toContain('b2bspastore app is running!');
+    expect(compiled.querySelector('h1')?.textContent).toContain('Hello, b2bspastore');
   });
 });
